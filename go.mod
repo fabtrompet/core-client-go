86a7f261b92c6680b2092efd1765a06a36e48269
Ingo Oppermann <ingo@datarhei.com>
2024-04-24 12:51:58 +0200

    Replace encoding/json with github.com/goccy/go-json

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require (
 	github.com/Masterminds/semver/v3 v3.2.1
 	github.com/gobwas/glob v0.2.3
+	github.com/goccy/go-json v0.10.2
 	github.com/golang-jwt/jwt/v4 v4.5.0
 	github.com/stretchr/testify v1.8.4
 )
--- process_test.go
+++ process_test.go
@@ -1,8 +1,13 @@
 package coreclient
 
 import (
+	"os"
 	"testing"
 
+	gojson "encoding/json"
+
+	"github.com/datarhei/core-client-go/v16/api"
+	"github.com/goccy/go-json"
 	"github.com/stretchr/testify/require"
 )
 
@@ -18,5 +23,27 @@ func TestParseProcessID(t *testing.T) {
 		ppid := ParseProcessID(pid)
 
 		require.Equal(t, id, ppid, pid)
+	}
+}
+
+func BenchmarkProcessList(b *testing.B) {
+	data, err := os.ReadFile("./fixtures/processList.json")
+	require.NoError(b, err)
+
+	for i := 0; i < b.N; i++ {
+		processes := []api.Process{}
+		err := json.Unmarshal(data, &processes)
+		require.NoError(b, err)
+	}
+}
+
+func BenchmarkProcessListNativJSON(b *testing.B) {
+	data, err := os.ReadFile("./fixtures/processList.json")
+	require.NoError(b, err)
+
+	for i := 0; i < b.N; i++ {
+		processes := []api.Process{}
+		err := gojson.Unmarshal(data, &processes)
+		require.NoError(b, err)
 	}
 }
